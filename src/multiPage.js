3b1e6e34fb3f8d13e24eb187b167d86bfdc15d5d
Jacob Ankney <crosswalkastronaut@Jacobs-MacBook-Pro-3.local>
2020-11-13 11:50:13 -0700

    fixed

--- src/multiPage.js
+++ src/multiPage.js
@@ -1,40 +1,39 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Document, Page, pdfjs } from "react-pdf";
-
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 const MultiPage = props => {
+  const [numPages, setNumPages] = useState(null);
 
-    const [numPages, setNumPages]=useState(null)
+  function onSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
 
+  const { pdf } = props;
 
-    function onSuccess({numPages}){
-        setNumPages(numPages)
-    }
-
-
-const {pdf}=props
-
-  return <div >
-
-    <Document 
-    
-    // options={{workerSrc:'../'}}
-    className='pdfHold'
-    file={pdf}
-    onLoadSuccess={onSuccess}
-    onSourceError={(err)=>console.log(err)}
-    
-    >
-
-{Array.from(new Array(numPages), (el, index)=>{
-    return <Page key={`page_${index+1}`} pageNumber={index+1} />
-})}
-
-    </Document>
-
-  </div>;
+  return (
+    <div>
+      <Document
+        // options={{workerSrc:'../'}}
+        className="pdfHold"
+        renderMode="svg"
+        file={pdf}
+        onLoadSuccess={onSuccess}
+        onSourceError={err => console.log(err)}
+      >
+        {Array.from(new Array(numPages), (el, index) => {
+          return (
+            <Page
+              key={`page_${index + 1}`}
+              pageNumber={index + 1}
+              width={350}
+            />
+          );
+        })}
+      </Document>
+    </div>
+  );
 };
 
 export default MultiPage;
